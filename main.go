acfcc090ccd55a1b8dcba126d7e1932cf748e30a
marcela <marcela>
2018-09-17 17:19:03 -0700

    ultimas modificaciones

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
+	"fmt"
 
-  "fmt"
-  "github.com/gin-gonic/gin"
-  "sait.mx/MVC/model"
-  "sait.mx/MVC/controllers"
-
+	"github.com/gin-gonic/gin"
+	"sait.mx/MVC/controllers"
+	"sait.mx/MVC/model"
 )
 
-func main(){
-  fmt.Println("Pruebas con bases de datos")
-  model.OpenDB()
+func main() {
+	fmt.Println("Pruebas con bases de datos")
+	model.OpenDB()
 
-  r := gin.Default()
-  r.GET("/api/v1/clientes", controllers.ListClientes)
-  r.GET("/api/v1/clientes/:id", controllers.GetCliente)
-  r.POST("/api/v1/clientes", controllers.InsertCliente)
-  r.PUT("/api/v1/clientes/:id", controllers.UpdateCliente)
-    r.DELETE("/api/v1/clientes/:id", controllers.DeleteCliente)
+	r := gin.Default()
+	r.GET("/api/v1/clientes", controllers.ListClientes)
+	r.GET("/api/v1/clientes/:id", controllers.GetCliente)
+	r.POST("/api/v1/clientes", controllers.InsertCliente)
+	r.PUT("/api/v1/clientes/:id", controllers.UpdateCliente)
+	r.DELETE("/api/v1/clientes/:id", controllers.DeleteCliente)
 
-  r.Run(":9053")
+	r.Run(":9053")
 }
